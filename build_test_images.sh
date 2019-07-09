1e0afec34c88ad573ac182ea81a44b230e323ac3
John Gruber <john.t.gruber@gmail.com>
2019-07-09 13:38:05 -0500

    made the phone_home_url dynamic in the build_test_images script

--- build_test_images.sh
+++ build_test_images.sh
@@ -10,7 +10,8 @@ internal_network_name=internal
 vip_network_name=external
 ltm_1slot_flavor=LTM.1SLOT
 all_1slot_flavor=ALL.1SLOT
-
+phone_home_url_view='https://webhook.site/#!/5a5310ff-648d-4dfc-97e3-6bbe9d88d111/9c8aeb41-0440-47da-b94e-75b2052aa6a2/1'
+phone_home_url='https://webhook.site/5a5310ff-648d-4dfc-97e3-6bbe9d88d111'
 openstack flavor list | grep LTM.1SLOT | cut -d' ' -f2
 
 echo "docker run --rm -it -v ${imagesDir}:/TMOSImages -v ${iControlLXPackagesDir}:/iControlLXPackages tmos_image_patcher:latest"
@@ -53,5 +54,8 @@ all_1slot_flavor_id=$(openstack flavor list | grep ${all_1slot_flavor} | cut -d'
 
 sed -i "/ tmos_flavor/c\  tmos_flavor: ${ltm_1slot_flavor_id}" *do_only_env.yaml
 sed -i "/ tmos_flavor/c\  tmos_flavor: ${all_1slot_flavor_id}" *waf_env.yaml
+
+sed -i "/ monitor at/c\  # monitor at: ${phone_home_url_view}" *_env.yaml
+sed -i "/ phone_home_url/c\  phone_home_url: ${phone_home_url}" *_env.yaml
 
 cd ${rtdir}
